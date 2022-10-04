1a05901281a965412a757ea510c847f251b5e61c
Bignichok <inikita546@gmai.com>
2022-10-04 09:19:12 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import { CsvFileReader } from "./CsvFileReader";
+
+const reader = new CsvFileReader("football.csv");
+reader.read();
+const matches = reader.data;
+console.log(matches);
+enum MatchResult {
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw = "D",
+}
