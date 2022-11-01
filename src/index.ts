dcc38a73e507daa941ba2a233d361c1181fda50b
bignichok <inikita546@gmail.com>
2022-11-01 16:34:52 +0200

    add static method to match reader

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
 
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reporters/ConsoleReport';
-import { HtmlReport } from './reporters/HtmlReport';
-
-const csvFileReader = new CsvFileReader('football.csv');
-
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
 // const summary = new Summary(new WinsAnalysis('Everton'), new ConsoleReport());
 // const htmlSummary = new Summary(new WinsAnalysis('Everton'), new HtmlReport());
 
-
 //OR
-const summary = Summary.winsAnalysisWithConsoleReport('Everton')
+const summary = Summary.winsAnalysisWithConsoleReport('Everton');
 const htmlSummary = Summary.winsAnalysisWithHtmlReport('Everton');
 
 summary.buildAndPrintReport(matchReader.matches);
